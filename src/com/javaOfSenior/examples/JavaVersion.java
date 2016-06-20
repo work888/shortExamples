cc9d2d7bd5d1b8f48f96a747990897b7cd89a760
work888 <work888@i.ua>
2016-06-20 19:19:23 -0400

    move to work888

---
+++ src/com/javaOfSenior/examples/JavaVersion.java
@@ -0,0 +1,7 @@
+package com.javaOfSenior.examples;
+
+public class JavaVersion {
+	  public static void main(String[] args) {
+	    System.out.println(System.getProperty("java.runtime.version"));
+	  }
+	}
